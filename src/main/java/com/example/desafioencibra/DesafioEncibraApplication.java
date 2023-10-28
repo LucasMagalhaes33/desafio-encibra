693f26be7f89010a9c3d660b8505de16c6d0bc8d
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-10-27 22:27:26 -0300

    feature: criando models.

---
+++ src/main/java/com/example/desafioencibra/DesafioEncibraApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioencibra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEncibraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEncibraApplication.class, args);
+	}
+
+}
